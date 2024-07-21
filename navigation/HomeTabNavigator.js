aa66f5a1046d59f4b60b84aaf5dd3945958c19eb
Syariqul <qullll479@gmail.com>
2024-07-22 02:26:12 +0800

    add tab icon

--- navigation/HomeTabNavigator.js
+++ navigation/HomeTabNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from '../screens/HomeScreen';
 import ItemsScreen from '../screens/ItemsScreen';
 import UsersScreen from '../screens/UsersScreen';
@@ -8,8 +9,29 @@ const Tab = createBottomTabNavigator();
 
 const HomeTabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} option={{ title: 'Home' }} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'HomeTab') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Items') {
+            iconName = focused ? 'list' : 'list-outline';
+          } else if (route.name === 'Users') {
+            iconName = focused ? 'people' : 'people-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    //   tabBarOptions={{
+    //     activeTintColor: 'tomato',
+    //     inactiveTintColor: 'gray',
+    //   }}
+    >
+      <Tab.Screen name="HomeTab" component={HomeScreen} options={{ title: 'Home' }} />
       <Tab.Screen name="Items" component={ItemsScreen} />
       <Tab.Screen name="Users" component={UsersScreen} />
     </Tab.Navigator>
